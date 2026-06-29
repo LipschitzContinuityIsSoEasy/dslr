2edb00d1d23d95022c73ea84dec90075f73b2516
LipschitzContinuityIsSoEasy <meiling.git@gmail.com>
2026-06-30 00:38:00 +0200

    test: stocker et imprimer dict_for_data

--- describe.py
+++ describe.py
@@ -2,7 +2,46 @@
 import sys
 import csv
 
+def clean_and_save_data(file_train):
+    dict_for_data = {}
+
+    with open(file_train, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        
+        # 1. obtenir le header
+        header = next(reader)
+
+        # 2. initialiser la clef, chaque header a une liste vide []
+        for col in header:
+            dict_for_data[col] = []
+        
+        # 3. boucle lire donnee
+        for row in reader:
+            # chaque row est une liste
+            # 0-eme correspond a 0-eme col
+            # n-eme correspond a n-eme col
+            for index, element in enumerate(row):
+                # trouver le col correspond par
+                # index de row et index de header en meme teps
+                header_name = header[index]
+                dict_for_data[header_name].append(element)
+        
+        # imprimer cette dict (premiers 3)
+        for k, v in list(dict_for_data.items())[:3]:
+            print(f"抽屉（表头）: {k} -> 里面的数据列表: {v[:5]} ... (共 {len(v)} 条)")
+
+
+    
+    return dict_for_data
+
+def describe(file_train):
+    dict_for_data = clean_and_save_data(file_train)
+    # stats_result = calculate_statistics(dict_for_data)
+    # display_statistics(stats_result)
+    pass
+
 def main():
+# etape: 1. creer un dictionaire {"colon1": [chiffre_1, chiffre_2], ..., "colon2":[chiffre1, chiffre2...], ...}
     args = sys.argv
 
     #  ??trop tard?
@@ -12,36 +51,11 @@ def main():
 
     file_train = args[1]
     try:
-        with open(file_train, "r", encoding="utf-8") as file:
-            reader = csv.reader(file)
-
-            print("--- les premiers 3 lignes, change , a | ---")
-            
-            # 1. obtenir la premiere ligne comme header
-            headers = next(reader)
-            for row_idx in range(3):
-                row_data = next(reader)
-                
-                print(f"\n[ 行号: {row_idx + 1} ]")
-                print("-" * 30) # 打印一条分隔线
-                
-                # imprimer header et les donnes corresponds
-                for col_idx, header in enumerate(headers):
-                    value = row_data[col_idx] if col_idx < len(row_data) else "N/A"
-                    
-                    # 关键点：
-                    # 1. header:<30 表示表头占用30个字符宽度，不足的用空格补齐
-                    # 2. replace(" ", "-") 把表头后面的空格全部替换成横线 -
-                    # 3. 紧接着打印 " | "
-                    # 4. 最后打印数值 value
-                    header_part = f"{header:<35}".replace(" ", "-")
-                    
-                    print(f"[{col_idx + 1:<2}] {header_part} | {value}")
-                print("-" * 30)
+        describe(file_train)
             
     except Exception as e:
         print(f"Erreur inattendue : {e}")
-        exit(1) 
+        exit(1)
 
 
 if __name__ == "__main__":
