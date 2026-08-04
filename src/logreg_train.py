389aab8bea03fb1b18f956416d71ff9e4ec2ceb8
LipschitzContinuityIsSoEasy <meiling.git@gmail.com>
2026-08-04 18:24:49 +0200

    train ok

--- src/logreg_train.py
+++ src/logreg_train.py
@@ -5,6 +5,8 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
+import csv
 
 from sklearn.feature_selection import f_classif
 
@@ -66,7 +68,7 @@ def load_and_preprocess_data(file_name):
     # return donnee: juste retouner les cols 'Hogwarts House' et les premiers 10 cols
     return clean_data[best_features + ['Hogwarts House']], best_features
 
-def normalize_data(cleaned_data):
+def normalize_data(cleaned_data, best_features):
     # 1. copier et coller
     normalized_copie = cleaned_data.copy()
 
@@ -74,7 +76,7 @@ def normalize_data(cleaned_data):
     houses = normalized_copie.pop('Hogwarts House')
 
     # 3. juste les premiere 10 caracteristiques
-    for col in normalized_copie.columns:
+    for col in best_features:
         mean_val = normalized_copie[col].mean()
         std_val = normalized_copie[col].std()
 
@@ -86,6 +88,102 @@ def normalize_data(cleaned_data):
     # 4. rajouter les houses
     normalized_copie['Hogwarts House'] = houses
 
+    return normalized_copie
+
+# OvR (One-vs-All)
+def train_single_house(normalized_data, best_features, target_house, learning_rate):
+    # 1. ici on utilsie batch gradient descent(BGD)
+    m = len(normalized_data)
+    num_features = len(best_features)
+
+    # initialiser tous en 0
+    weights = [0.0] * num_features
+    bias = 0.0
+
+    for _ in range(30000):
+        # Batch Gradient Descent (BGD)
+        # La Descente de Gradient par Lot
+
+        # initialiser les accumulateurs
+        sum_error_weights = [0.0] * num_features
+        sum_error_bias = 0.0
+
+        # # stocker l'érreur carrée totale pour cet itération
+        # current_sum_mse = 0.0
+
+        for i in range(m):
+            # 0. obtenir les donnees d'un eleve en i-eme ligne
+            row = normalized_data.iloc[i]
+
+            # 1. calculer score lineaire z = b + w1*x1 + w2*x2 + ..
+            z = bias
+            for j in range(num_features):
+                feature_name = best_features[j]
+                z += weights[j] * row[feature_name]
+
+            # 2. remplacer dans Sigmoid pour avoir le resultat
+            prediction = 1.0 / (1.0 + math.exp(-z))
+
+            # 3. savoir le vrai label y pour l'eleve actuel (0 ou 1)
+            real_house = row['Hogwarts House']
+            y = 0.0
+            if (real_house == target_house):
+                y = 1.0
+
+            # 4. calculer l'error
+            error = prediction - y
+
+            # 5. accumuler les gradients
+            sum_error_bias += error
+            for j in range(num_features):
+                feature_name = best_features[j]
+                sum_error_weights[j] += error * row[feature_name]
+
+        # 6. apres une boucle, renouveler tout en meme temps
+        bias = bias - (learning_rate * (1/m) * sum_error_bias)
+        for j in range(num_features):
+            weights[j] = weights[j] - (learning_rate * (1 / m) * sum_error_weights[j])
+
+    return weights, bias
+
+def save_weights_to_csv(all_parameters, filename):
+    try:
+        with open(filename, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            
+            # 1. headers (House, Bias, W0, W1, ... W9)
+            first_house_data = next(iter(all_parameters.values()))
+            num_features = len(first_house_data['weights'])
+            header = ['House', 'Bias'] + [f'W_{i}' for i in range(num_features)]
+            writer.writerow(header)
+            
+            # 2. ecrire les donnees dans le fichier
+            for house, params in all_parameters.items():
+                row = [house, params['bias']] + params['weights']
+                writer.writerow(row)
+                
+        print(f"Poids sauvegardés avec succès dans {filename} !")
+
+    except IOError as e:
+        print(f"Erreur lors de l'écriture du CSV : {e}")
+        exit(1)
+
+def train_all_houses(normalized_data, best_features, learning_rate):
+    houses = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff']
+    filename="weights.csv"
+
+    all_parametres = {}
+
+    for house in houses:
+        print(f"Entraînement du modèle pour : {house}...")
+        weights, bias = train_single_house(normalized_data, best_features, house, learning_rate)
+
+        all_parametres[house] = {'weights': weights, 'bias': bias}
+
+    # ecrire dans un csv
+    save_weights_to_csv(all_parametres, filename)
+    print("Tous les modèles sont entraînés avec succès !")
+    return all_parametres
 
 def logreg_train(file_name):
     # 1. faire 1 et 2
@@ -94,10 +192,12 @@ def logreg_train(file_name):
     # 2. faire 3 normalisation
     # =====================================================================================================================================================
     # attention: ici on utilise Z-score(x-niu)/sigma, pas (x-min)/(max-min)
-    normalized_data = normalize_data(cleaned_data)
+    normalized_data = normalize_data(cleaned_data, best_features)
+
+    learning_rate = 0.1
 
     # 3. boucle et aussi stocker dans un fichier a la fin
-    # train_all_houses(normalized_data, best_features)
+    train_all_houses(normalized_data, best_features, learning_rate)
 
 # 1. lire les donnee, stocker dans DataFrame dans Pandas, et nettoyer
 # 2. F-score, choisir les premieres 10
